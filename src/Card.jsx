8bd8f359b6ce39919f1b444ada4c6f7e349f24fb
pratishu <pratishu.kr@gmail.com>
2025-03-16 10:50:20 +0530

    motion implimentation on card

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,6 +8,8 @@ const Card = ({ title, id, column, handleDragStart }) => {
       {/* we are making drop indicator line with div and only will activated when cards will hover over it: other function in column are implimenting that */}
       <DropIndicator beforeId={id} column={column} />
       <motion.div
+        layout
+        layoutId={id}
         draggable="true"
         onDragStart={(e) => handleDragStart(e, { title, id, column })}
         className="cursor-grab rounded-lg border-2 border-gray-500 bg-zinc-800/20 p-3 active:cursor-grabbing"
