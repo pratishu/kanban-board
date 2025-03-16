b6d5de7d10495a41c78a8d5bc1b84b88a8b2d04e
pratishu <pratishu.kr@gmail.com>
2025-03-16 09:58:05 +0530

    board.jsx completed

---
+++ src/Board.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Column } from "./Column";
+import { DEFAULT_CARDS } from "./Resources/jsondata";
+
+export const Board = () => {
+  const [cards, setcards] = useState(DEFAULT_CARDS);
+  return (
+    <div className="flex h-full w-full gap-8 overflow-scroll justify-center">
+      <Column
+        title="Backlog"
+        column="backlog"
+        cards={cards}
+        setcards={setcards}
+        headingcolor="text-gray-200"
+      />
+      <Column
+        title="TODO"
+        column="todo"
+        cards={cards}
+        setcards={setcards}
+        headingcolor="text-yellow-500"
+      />
+
+      <Column
+        title="In Progress"
+        column="doing"
+        cards={cards}
+        setcards={setcards}
+        headingcolor="text-green-500"
+      />
+      <Column
+        title="Completed"
+        column="done"
+        cards={cards}
+        setcards={setcards}
+        headingcolor="text-blue-500"
+      />
+    </div>
+  );
+};
