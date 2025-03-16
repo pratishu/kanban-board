a27fdf23ecc39e6c1644b473786bc5b4ff5003d7
pratishu <pratishu.kr@gmail.com>
2025-03-16 06:13:37 +0530

    Tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return <>everything cleared here</>;
+  return (
+    <div
+      className="text-8xl text-blue-500 text-center bg-blue-200 leading-loose
+    "
+    >
+      tailwind is working now
+    </div>
+  );
 }
 
 export default App;
